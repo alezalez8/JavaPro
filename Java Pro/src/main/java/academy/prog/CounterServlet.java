9dc8845f6039815d3d60a5fad8318b640fe051b9
alezalez <5446943a@gmail.com>
2022-06-23 21:09:44 +0300

    Initial commit

---
+++ Java Pro/src/main/java/academy/prog/CounterServlet.java
@@ -0,0 +1,14 @@
+package academy.prog;
+
+import jakarta.servlet.http.*;
+import java.io.IOException;
+
+@SuppressWarnings("serial")
+public class CounterServlet extends HttpServlet {
+	int x = 0;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		//int x = 0;
+		resp.getWriter().println("<html><h1>" + (x++) + "</h1></html>");
+	}
+}
